fc8dd9199241fad11332e80bf2e3023738187d02
Rhodri Davies <hv23625@bristol.ac.uk>
2025-04-10 23:37:01 +0100

    Script to run slurm job

---
+++ run_slurm_job.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=mldm_summative
+#SBATCH --output=./slurm_logs/output.txt
+#SBATCH --error=./slurm_logs/output.txt
+#SBATCH --partition=teach_cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:2
+#SBATCH --time=0:30:0
+#SBATCH --mem=1G
+#SBATCH --account=SSCM033324
+
+mkdir -p slurm_logs
+
+echo 'Activating conda environment'
+source /user/work/hv23625/miniforge3/etc/profile.d/conda.sh
+conda deactivate
+conda activate mldm-env
+conda info --envs
+
+echo 'Running python script'
+which python
+python main.py
+
+echo 'Slurm job finished'
