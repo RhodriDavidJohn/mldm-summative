7e36cab65b3db0a2ec68888b1753bf605ae981c8
= <=>
2025-04-10 15:47:37 +0100

    Adding logger and run_id setup

--- code/utils/helpers.py
+++ code/utils/helpers.py
@@ -2,15 +2,53 @@
 import os
 import pandas as pd
 import logging
+from datetime import datetime
 import sys
 
 
-def setup_logger(folder: str) -> logging.Logger:
-    pass
+def setup_logger(run_id: str, folder: str) -> logging.Logger:
+    
+    logging_file = os.path.join(folder, f"{run_id}.log")
+    os.makedirs(os.path.dirname(logging_file), exist_ok=True)
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(messages)s',
+                        datefmt='%m-%d %H:%M',
+                        filename=logging_file,
+                        filemode='w')
+    
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+
+    console.setFormatter(formatter)
+
+    logging.getLogger('').addHandler(console)
+
+    LOGGER = logging.getLogger(f'{run_id}.logger')
+
+    return LOGGER
 
 
 def create_run_id(folder: str) -> str:
-    pass
+
+    date = datetime.now().strftime("%Y-%m-%d") + "_"
+
+    try:
+        prev_id = os.listdir(folder)[0]
+        prev_date = prev_id[0:11]
+        if prev_date == date:
+            run_id = int(prev_id[-6:-4]) + 1
+        else:
+            run_id = 1
+    except Exception as e:
+        run_id = 1
+
+    run_id = str(run_id).zfill(2)
+    run_id = date + run_id
+
+    return run_id
 
 
 def load_csv(filepath: str, LOGGER: logging.Logger) -> pd.DataFrame:
