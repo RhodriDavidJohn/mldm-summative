da59d1bb9b8cac5ff706280d58a1523c8473c246
= <=>
2025-04-15 21:56:26 +0100

    Download metadata to allow more efficitent image data download

--- code/modules/DownloadData.py
+++ code/modules/DownloadData.py
@@ -17,21 +17,43 @@ class DownloadData:
     
     def download_data(self):
         """"
-        Download both clinical and (PET)CT data from the shared HPC area
+        Download both clinical and image data from the shared HPC area
         """
+
+        self.LOGGER.info('Downloading metadata')
+        self.metadata1 = self.download_metadata('dataset1', 'metadata1.csv')
+        self.metadata2 = self.download_metadata('dataset2', 'metadata2.csv')
 
         self.LOGGER.info('Downloading clinical data')
         self.clinical1 = self.download_clinical_data('dataset1', 'clinical1.csv')
         self.clinical2 = self.download_clinical_data('dataset2', 'clinical2.csv')
 
-        self.LOGGER.info('Downloading (PET)CT images')
-        self.image_data_dict = self.download_ct_data(segmented=False)
-
-        self.LOGGER.info('Downloading (PET)CT segmented images')
-        self.seg_image_data_dict = self.download_ct_data(segmented=True)
+        self.LOGGER.info('Downloading CT and segmented images')
+        self.image_data_dict = self.download_images()
 
 
 
+    def download_metadata(self, dataset: str, filename: str) -> pd.DataFrame:
+        """
+        Download the metadata for each dataset from the shared HPC area
+        """
+
+        # load the data
+        input_filepath = os.path.join(self.input_path, dataset, 'metadata.csv')
+
+        df = hlp.load_csv(input_filepath, self.LOGGER)
+
+        # save the data
+        if not os.path.exists(os.path.join(self.output_path, 'metadata')):
+            os.makedirs(os.path.join(self.output_path, 'metadata'))
+
+        output_filepath = os.path.join(self.output_path, 'metadata', filename)
+
+        hlp.save_csv(df, input_filepath, output_filepath, self.LOGGER)
+
+        return df
+
+    
     def download_clinical_data(self, dataset: str, filename: str) -> pd.DataFrame:
         """
         Download the clinical csv data from the shared HPC area
@@ -53,78 +75,96 @@ class DownloadData:
         return df
 
 
-    def download_ct_data(self, segmented: bool) -> dict:
+    def download_images(self) -> dict:
         """
-        Download the (PET)CT image data from the shared HPC area
+        Download the CT images and segmented images from the shared HPC area
         """
 
-        if segmented:
-            folder_filter = 'Segmentation'
-            image_filename = 'seg_ct_image.png'
-            image_type = 'Segmented image'
-        else:
-            folder_filter = '1.00'
-            image_filename = 'orig_ct_image.png'
-            image_type = 'Image'
+        meta1 = self.metadata1
+        meta1['Dataset'] = 'dataset1'
+        meta2 = self.metadata2
+        meta2['Dataset'] = 'dataset2'
+        metadata = pd.concat([meta1, meta2], axis=0)
+
+        seg_ids = metadata.loc[metadata['Modality']=='SEG', 'Subject ID'].unique().tolist()
 
         image_data_dict = {}
 
-        input_filepath = os.path.join(self.input_path, 'dataset1')
-        output_directory = os.path.join(self.output_path, 'ct_image')
+        output_directory = os.path.join(self.output_path, 'images')
 
-        for patient_id in os.listdir(input_filepath):
-            patient_folder = os.path.join(input_filepath, patient_id)
+        for patient_id in seg_ids:
 
-            # check if the item is a folder because
-            # files exist in the directory
-            if not os.path.isdir(patient_folder):
-                continue
+            seg_filepath = self.get_image_filepath(metadata, patient_id, 'SEG')
+            ct_filepath = self.get_image_filepath(metadata, patient_id, 'CT')
             
-            # the patient folder should only contain one item
-            # which is a subfolder
-            try:
-                assert len(os.listdir(patient_folder))==1
-            except Exception as e:
-                msg = (f"{patient_id} has {len(os.listdir(patient_folder))} "
-                       f"subfolders instead of 1. The following error was raised: {e}")
-                self.LOGGER.error(msg)
-                raise(e)
-
-            subfolder = os.path.join(patient_folder, os.listdir(patient_folder)[0])
-
-            assert os.path.isdir(subfolder)
-
-            # the subfolder should contain 3 more folders
-            # but we only want the one with the segmented image
-            for dir in os.listdir(subfolder):
-                if folder_filter in dir:
-                    folder = os.path.join(subfolder, dir)
-                    break
-                
-
-            # read the image file
-            assert len(os.listdir(folder))==1
-            assert os.listdir(folder)[0].endswith('.dcm')
-
-            file = os.path.join(folder, os.listdir(folder)[0])
-            image = hlp.load_dicom(file, self.LOGGER)
-
-
-            # save the image as jpeg
-            patient_save_path = os.path.join(output_directory, patient_id)
-            if not os.path.exists(patient_save_path):
-                os.makedirs(patient_save_path)
+            image_type_dict = {}
             
-            image_save_loc = os.path.join(patient_save_path, image_filename)
-            hlp.save_medical_image(image, f"{image_type} for {patient_id}",
-                                   image_save_loc, self.LOGGER)
+            # load the images
+            image_type_dict['seg'] = self.load_images(seg_filepath)
+            image_type_dict['ct'] = self.load_images(ct_filepath)
+
+            # save the images as tif files
+            seg_savepath = os.path.join(output_directory, patient_id, 'segmented')
+            ct_savepath = os.path.join(output_directory, patient_id, 'ct')
+            self.save_images(image_type_dict['seg'], seg_savepath, patient_id)
+            self.save_images(image_type_dict['ct'], ct_savepath, patient_id)
 
             # populate the data dictionary
-            image_data_dict[patient_id] = image
+            image_data_dict[patient_id] = image_type_dict
         
-        msg = (f"Downloaded {len(image_data_dict)} {image_type.lower()}s "
+        msg = (f"Downloaded images for {len(image_data_dict)} patients "
                f"and saved to {output_directory}")
         self.LOGGER.info(msg)
         
         return image_data_dict
 
+
+    def get_image_filepath(self, df: pd.DataFrame, patient_id: str, image_type: str) -> str:
+        """
+        Get the filepath to the image(s)
+        """
+
+        def access_dataframe_value(column: str) -> str:
+
+            value = (
+                df
+                .loc[(df['Subject ID']==patient_id)&(df['Modality']==image_type),
+                     column]
+                .values[0]
+            )
+            return value
+
+        dataset = access_dataframe_value('Dataset')
+        filepath = access_dataframe_value('File Location')
+
+        return os.path.join(self.input_path, dataset, filepath)
+    
+
+    def load_images(self, filepath: str) -> list:
+        """
+        Load the images from the given filepath
+        """
+
+        images = []
+        for file in os.listdir(filepath):
+            file = os.path.join(filepath, file)
+            image = hlp.load_dicom(file, self.LOGGER)
+            images.append(image)
+        
+        return images
+    
+
+    def save_images(self, images: list, folderpath: str, patient_id: str) -> None:
+        """
+        Save the images to the given filepath
+        """
+
+        if not os.path.exists(folderpath):
+            os.makedirs(folderpath)
+        
+        for i, image in enumerate(images):
+            image_filename = f"image_{i}.tif"
+            image_save_loc = os.path.join(folderpath, image_filename)
+            hlp.save_medical_image(image, f"Image for {patient_id}",
+                                   image_save_loc, self.LOGGER)
+        return None
